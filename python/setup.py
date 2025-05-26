72f79aab2c9f9cee8db1aadea297c83a032df72d
simon <simon.hobbs@electrooptical.net>
2025-05-26 16:56:14 -0400

    mergingsetup.py changes

--- python/setup.py
+++ python/setup.py
@@ -60,9 +60,10 @@ setup(
   package_data={'openEMS': ['*.pxd']},
   python_requires='>=3.9',
   install_requires=[
-    'cython', # Apache License 2.0 (https://github.com/cython/cython/blob/master/LICENSE.txt)
-    'h5py',   # BSD 3-Clause (https://github.com/h5py/h5py/blob/master/LICENSE)
-    'numpy',  # BSD 3-Clause (https://github.com/numpy/numpy/blob/main/LICENSE.txt)
+    'cython>=3.0.6', # Apache License 2.0 (https://github.com/cython/cython/blob/master/LICENSE.txt)
+    'pkgconfig',
+    'h5py==3.13.0',   # BSD 3-Clause (https://github.com/h5py/h5py/blob/master/LICENSE)
+    'numpy==1.26.2',  # BSD 3-Clause (https://github.com/numpy/numpy/blob/main/LICENSE.txt)
   ],
   ext_modules = cythonize(extensions, language_level = 3)
  )
